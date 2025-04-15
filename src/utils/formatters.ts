ca6355ce34ec743103efc8161641acd9e8f59943
Radomir Skrzepij <zyziu10@gmail.com>
2025-04-15 10:23:36 +0200

    reformat formatters file

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -5,11 +5,11 @@
  */
 export const formatDate = (dateString: string): string => {
   // format dateString to DD-MM-YYYY
-    const date = new Date(dateString);
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    return `${day}-${month}-${year}`;
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}-${month}-${year}`;
 };
 
 /**
